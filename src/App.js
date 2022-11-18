d2a2393c11e19c78f6acbaa1a7008e739d43b257
EfeBDN <64277179+EfeBDN@users.noreply.github.com>
2022-11-18 19:51:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ButtonAppBar from "./components/buttonAppBar";
 
 import { Login } from "./pages/login";
 import ReservasLibros from "./pages/reservaLibros";
+import ReservasSalas from "./pages/reservaSalas";
 import Resumen from "./pages/resumen";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/resumen" element={<Resumen />} />
         <Route path="/reservas-libros" element={<ReservasLibros />} />
+        <Route path="/reservas-salas" element={<ReservasSalas />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
