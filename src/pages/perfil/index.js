81eb403f38ca8cc8ca6f0d806e94516c110c85eb
Barbara <50504493+BarbaraPB@users.noreply.github.com>
2022-12-03 19:44:49 -0300

    perfil

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,12 +1,12 @@
 import { Avatar, Box, Button, Card, Grid, Typography } from "@mui/material";
-import { blue, grey, pink, red } from "@mui/material/colors";
+import { blue, green, grey, pink, purple, red } from "@mui/material/colors";
 import React, { Component } from "react";
 import Usuario from "../../img/usuario.png";
 
 export default class Perfil extends Component {
   render() {
     return (
-      <Box sx={{ backgroundColor: grey[200], padding: 8 }}>
+      <Box sx={{ height: "100%", backgroundColor: grey[200], padding: 8 }}>
         {/* titulo */}
         <Grid justifyItems="center" sx={{ marginBottom: 4 }}>
           <Typography
@@ -26,11 +26,12 @@ export default class Perfil extends Component {
           justifyItems="center"
           container
           direction="row"
-          spacing={5}
           sx={{ marginTop: 4 }}
         >
           <Card
             sx={{
+              display: "flex",
+              flexDirection: "row",
               marginX: 15,
               height: 400,
               width: "100vw",
@@ -39,18 +40,48 @@ export default class Perfil extends Component {
             {/* lado izquierdo (foto) */}
             <Grid
               item
-              justifyContent="center"
-              alignItems="center"
               xs={5}
-              sx={{ backgroundColor: red[200], height: "100vh" }}
+              sx={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                paddingRight: 10,
+              }}
             >
-              <Grid>
-                <Avatar minHeight={200} src={Usuario} />
-              </Grid>
+              <Avatar
+                sx={{
+                  variant: "rounded",
+                  height: 150,
+                  width: 150,
+                }}
+                src={Usuario}
+              />
             </Grid>
             {/* lado derecho (info) */}
-            <Grid item xs={7} sx={{ backgroundColor: blue[200] }}>
-              <h1>bbbb</h1>
+            <Grid
+              item
+              xs={7}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              {/* titulos */}
+              <Grid sx={{ paddingLeft: 4, width: 150 }}>
+                <h4>Nommbre</h4>
+                <h4>Rut</h4>
+                <h4>Carrera</h4>
+                <h4>Correo</h4>
+              </Grid>
+              {/* info */}
+              <Grid>
+                <h4>Juanito Perez Diaz</h4>
+                <h4>12.345.678-9</h4>
+                <h4>ICCI</h4>
+                <h4>jperez@alumnos.ucn.cl</h4>
+              </Grid>
             </Grid>
           </Card>
         </Grid>
